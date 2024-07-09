0b3acc3f77483b7b44285de5934373e53cdb3315
brahmdi <hmdibrahim09@gmail.com>
2024-07-09 22:52:28 +0100

    tp 6

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/services/AccountService.java
@@ -0,0 +1,9 @@
+package org.sid.bankaccountservice.services;
+
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+
+public interface AccountService {
+    public BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+    public BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO bankAccountDTO);
+}
